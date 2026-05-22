2a9e87b9c3a6be7dac8500bc69f5cd928f443a6b
labcabrera <lab.cabrera@gmail.com>
2026-05-22 14:35:43 +0200

    Updated images and locales.

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -9,12 +9,17 @@ CLOUDFRONT_DISTRIBUTION_ID=EZMJSRCUQG2MM
 
 S3_TARGET="s3://$S3_BUCKET/$S3_PREFIX"
 
-# aws s3 rm "$S3_TARGET" --recursive --region "$AWS_REGION"
+# remove
 
+#aws s3 rm "$S3_TARGET"/images --recursive --region "$AWS_REGION"
 aws s3 rm "$S3_TARGET"/locales --recursive --region "$AWS_REGION"
 
-# aws s3 cp --recursive ./fonts "$S3_TARGET"/fonts --region "$AWS_REGION"
-aws s3 cp --recursive ./images "$S3_TARGET"/images --region "$AWS_REGION"
+#aws s3 cp --recursive ./fonts "$S3_TARGET"/fonts --region "$AWS_REGION"
+
+# add
+
+#aws s3 cp --recursive ./images "$S3_TARGET"/images --region "$AWS_REGION"
+
 aws s3 cp --recursive ./locales "$S3_TARGET"/locales --region "$AWS_REGION"
 
 aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_DISTRIBUTION_ID" --paths "/*"

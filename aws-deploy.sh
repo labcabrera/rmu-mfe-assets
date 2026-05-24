334b7fd978bf4d6d08a61eb582c5747cfeb35fe7
labcabrera <lab.cabrera@gmail.com>
2026-05-24 19:04:13 +0200

    Updated locales and deploy script.

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
+set -a
+source .env
+set +a
 
 S3_BUCKET=${S3_BUCKET:-rmu-static-assets-pro}
 S3_PREFIX=${S3_PREFIX:-rmu-mfe-assets}
 AWS_REGION=${AWS_REGION:-eu-west-1}
 
-CLOUDFRONT_DISTRIBUTION_ID=EZMJSRCUQG2MM
-
 S3_TARGET="s3://$S3_BUCKET/$S3_PREFIX"
 
-# remove
+# remove all
 
-#aws s3 rm "$S3_TARGET"/images --recursive --region "$AWS_REGION"
+# aws s3 rm "$S3_TARGET"/images --recursive --region "$AWS_REGION"
 aws s3 rm "$S3_TARGET"/locales --recursive --region "$AWS_REGION"
 
-#aws s3 cp --recursive ./fonts "$S3_TARGET"/fonts --region "$AWS_REGION"
+# add all
 
-# add
-
-#aws s3 cp --recursive ./images "$S3_TARGET"/images --region "$AWS_REGION"
-
+# aws s3 cp --recursive ./images "$S3_TARGET"/images --region "$AWS_REGION"
 aws s3 cp --recursive ./locales "$S3_TARGET"/locales --region "$AWS_REGION"
+
+# invalidate cloudfront cache
 
 aws cloudfront create-invalidation --distribution-id "$CLOUDFRONT_DISTRIBUTION_ID" --paths "/*"

894dc11d8e712ad00a2b9bc4c040e770cdf3d742
labcabrera <lab.cabrera@gmail.com>
2026-07-11 17:46:26 +0200

    Updated images.

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -13,12 +13,12 @@ S3_TARGET="s3://$S3_BUCKET/$S3_PREFIX"
 
 # remove all
 
-# aws s3 rm "$S3_TARGET"/images --recursive --region "$AWS_REGION"
+aws s3 rm "$S3_TARGET"/images --recursive --region "$AWS_REGION"
 aws s3 rm "$S3_TARGET"/locales --recursive --region "$AWS_REGION"
 
 # add all
 
-# aws s3 cp --recursive ./images "$S3_TARGET"/images --region "$AWS_REGION"
+aws s3 cp --recursive ./images "$S3_TARGET"/images --region "$AWS_REGION"
 aws s3 cp --recursive ./locales "$S3_TARGET"/locales --region "$AWS_REGION"
 
 # invalidate cloudfront cache
